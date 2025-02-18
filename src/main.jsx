40a93416b5927f03173de53ae643cf89e9cbe567
jennie <jennelyn.hernandez2578@gmail.com>
2025-02-18 17:43:31 +0800

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/slack-app-2025">
       <App />
     </BrowserRouter>
   </StrictMode>,
